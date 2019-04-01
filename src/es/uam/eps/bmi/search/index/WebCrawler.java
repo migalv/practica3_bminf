f5a8c4e4a60a57b468db6763d0b44d8d575e5fc9
Sergio Romero <sromero6797@gmail.com>
2019-04-01 18:19:44 +0200

    Juntado con la parte 2 y 3.

--- src/es/uam/eps/bmi/search/index/WebCrawler.java
+++ src/es/uam/eps/bmi/search/index/WebCrawler.java
@@ -1,279 +1,299 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *
+ * Fichero WebCrawler.java.
+ *
+ *
+ * @version 1.0
+ *
+ * Created on 15/03/2019
  */
 package es.uam.eps.bmi.search.index;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Queue;
-import java.util.Scanner;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import javax.net.ssl.HttpsURLConnection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 /**
+ * Clase WebCrawler encargada de rastrear por la web
  *
- * @author sergio
+ * @author Miguel Alvarez Lesmes
+ * @author Sergio Romero Tapiador
+ *
  */
 public class WebCrawler {
 
+    //Inicializacion de variables
     IndexBuilder index;
     int maxDocuments;
     String docPath;
-    Queue<String> pagesQueue;
-    ArrayList<String> list;
+    Queue<String> pagesPriorityQueue;
+    Queue<String> pagesLastQueue;
+    ArrayList<String> hostList;
+    ArrayList<String> indexList;
+    BufferedWriter writeLink = null;
 
+    /**
+     * Constructor de WebCrawler
+     *
+     * @param docPath path donde se encuentran las URLs semilla
+     * @param maxDocuments numero maximo de documentos a rastrear
+     * @param index indice del rastreador
+     *
+     * @throws IOException
+     * @throws MalformedURLException
+     */
     public WebCrawler(String docPath, int maxDocuments, IndexBuilder index) throws IOException, MalformedURLException {
         this.maxDocuments = maxDocuments;
         this.docPath = docPath;
         this.index = index;
-        this.pagesQueue = new LinkedBlockingQueue<>();
-        this.list = new ArrayList<>();
 
+        //Inicializamos dos cola de las urls no rastreadas (una con mas prioridad que la otra) 
+        this.pagesPriorityQueue = new LinkedBlockingQueue<>();
+        this.pagesLastQueue = new LinkedBlockingQueue<>();
+
+        //Inicializamos una lista con hosts
+        this.hostList = new ArrayList<>();
+
+        //Inicializamos una lista con las paginas que se puedan indexar
+        this.indexList = new ArrayList<>();
+
+        //Rastreamos las URLs
         this.startCrawling();
 
+        //Finalmente indexamos los documentos rastreados
+        this.indexar();
     }
 
+    /**
+     * Funcion encargada de manejar las URL con llamadas de busqueda a crawl
+     *
+     * @throws IOException
+     * @throws MalformedURLException
+     */
     private void startCrawling() throws IOException, MalformedURLException {
         String url;
+        boolean queue = false;
 
-        //Leemos las URLs semilla para empezar a rastrear
+        //Leemos las URLs semilla del fichero para empezar a rastrear
         try (BufferedReader reader = new BufferedReader(new FileReader(this.docPath))) {
             while ((url = reader.readLine()) != null) {
-                if (!this.list.contains(url)) {
-                    this.pagesQueue.add(url);
-                    this.list.add(url);
+                if (!this.indexList.contains(url)) {
+                    //Comprobamos que la URL sea correcta
+                    url = this.urlConnect(url);
+
+                    //En caso de que sea correcta lo añadimos a la lista de indexacion
+                    if (url == null) {
+                        break;
+                    }
+
+                    this.indexList.add(url);
+
+                    //Lo añadimos a la cola de prioridad
+                    this.pagesPriorityQueue.add(url);
+                    queue = true;
                 }
             }
         }
+
+        //Creamos el fichero donde guardaremos las URLs rastreadas
+        writeLink = new BufferedWriter(new FileWriter("graph" + File.separator + "web-Google.txt"));
 
         //Llamamos a la funcion crawl para que inspeccione la pagina y obtenga
         //los enlaces a los que apunta la url que queremos analizar
-        for (int i = 0; i < 10000; i++) {
-            if (this.list.size() > 4000) {
-                try (BufferedWriter writer = new BufferedWriter(new FileWriter("pages.txt"))) {
-                    for (String page : this.list) {
-                        writer.write(page + "\n");
-                    }
-                }
-                return;
+        while (queue == true && (this.maxDocuments > this.indexList.size())) {
+            //Sacamos la url de la cola y la rastreamos
+            if (!this.pagesPriorityQueue.isEmpty()) {
+                url = this.pagesPriorityQueue.poll();
+            } else {
+                url = this.pagesLastQueue.poll();
             }
-            url = this.pagesQueue.poll();
-            this.crawl(url);
+            if (url != null) {
+                this.crawl(url);
+            }
+
+            //Comprobamos que aun tenemos paginas por rastrear
+            queue = (!this.pagesPriorityQueue.isEmpty()) | (!this.pagesLastQueue.isEmpty());
         }
 
-        System.out.println(this.list.size());
+        //Cerramos el fichero
+        writeLink.close();
 
     }
 
+    /**
+     * Funcion encargada de encontrar los links de una URL dada
+     *
+     * @param url url de donde buscaremos los links
+     *
+     * @throws MalformedURLException
+     * @throws IOException
+     */
     private void crawl(String url) throws MalformedURLException, IOException {
         //Inicializacion de variables
-        ArrayList links = new ArrayList();
-        String[] splitter;
-        String urlFinal, content = null;
-        URL urlConnection;
+        String urlFinal;
 
-        try {
-            
-            //Comprobamos que la URL tenga protocolo y sea correcta
-            try {
-                urlConnection = new URL(url);
-            } catch (MalformedURLException ex) {
-                return;
-            }
-
-           /* if (url.contains("https://")) {
-                System.out.println("SS");
-            }*/
-
-            //Si se trata de una pagina https
-            if (url.contains("https")) {
-                //Nos conectamos a la URL
-                HttpsURLConnection connection;
-                connection = (HttpsURLConnection) urlConnection.openConnection();
-                
-                //Solo accedemos a aquellas que dan un codigo de respuesta en la que podamos leer informacion
-                //Nos puede salir tambien un error de que no existe la URL
-                try {
-                    int code = connection.getResponseCode();
-                    if (code >= 400 && code < 500) {
-                        return;
-                    }
-                } catch (IOException ex) {
-                    return;
-                }
-
-                //Obtenemos el contenido de la pagina
-                try (Scanner scanner = new Scanner(connection.getInputStream())) {
-                    scanner.useDelimiter("\\Z");
-                    if (scanner.hasNext()) {
-                        content = scanner.next();
-                    } else {//Si no hay contenido a leer
-                        return;
-                    }
-                }
-
-            } else {//Si se trata de una pagina http
-                //Nos conectamos a la URL
-                HttpURLConnection connection;
-                connection = (HttpURLConnection) urlConnection.openConnection();
-
-                //Solo accedemos a aquellas que dan un codigo de respuesta en la que podamos leer informacion
-                //Nos puede salir tambien un error de que no existe la URL
-                try {
-                    int code = connection.getResponseCode();
-                    if (code >= 400 && code < 500) {
-                        return;
-                    }
-                } catch (IOException ex) {
-                    return;
-                }
-
-                //Obtenemos el contenido de la pagina
-                try (Scanner scanner = new Scanner(connection.getInputStream())) {
-                    scanner.useDelimiter("\\Z");
-                    if (scanner.hasNext()) {
-                        content = scanner.next();
-                    } else {//Si no hay contenido a leer
-                        return;
-                    }
-                }
-            }
-
-        } catch (IOException ex) {
+        if (this.urlConnect(url) == null) {
             return;
         }
 
-        /*  Si se ha llegado a este punto, es que se ha obtenido el codigo html de una
-            URL , por lo que podemos buscar en ella los enlaces a los que apunta*/
-        
-        //En primer lugar, parseamos y obtenemos las paginas que esten formadas por http, https o www
-        String regex = "\\(?\\b(http://|https://|www[.])[-A-Za-z0-9+&amp;@#/%?=~_()|!:,.;]*[-A-Za-z0-9+&amp;@#/%=~_()|]";
-        Pattern p = Pattern.compile(regex);
-        Matcher m = p.matcher(content);
+        //En primer lugar, parseamos y obtenemos los links de tipo 'href'
+        Document doc;
+        try {
+            doc = Jsoup.connect(url).get();
+        } catch (IOException ex) {
+            return;
+        }
+        Elements links = doc.select("a[href]");
 
         //Lectura de todos los enlaces a los que apunta la URL actual
-        while (m.find()) {
-            //Detectamos un enlace posible
-            String urlStr = m.group();
-            if (urlStr.startsWith("(\") &amp;&amp; urlStr.endsWith(\")")) {
-                urlStr = urlStr.substring(1, urlStr.length() - 1);
-            }
+        for (Element link : links) {
+            //Con atributo 'href'
+            urlFinal = link.attr("abs:href");
 
-            //Realizamos un parseo y obtenemos las urls con enlace directo a la web principal
-            if (urlStr.contains(".es/")) {
-                splitter = urlStr.split(".es/");
-                urlFinal = splitter[0].concat(".es");
-            } else if (urlStr.contains(".com/")) {
-                splitter = urlStr.split(".com/");
-                urlFinal = splitter[0].concat(".com");
-            } else if (urlStr.contains(".fr/")) {
-                splitter = urlStr.split(".fr/");
-                urlFinal = splitter[0].concat(".fr");
-            } else if (urlStr.contains(".org/")) {
-                splitter = urlStr.split(".org/");
-                urlFinal = splitter[0].concat(".org");
-            } else if (urlStr.contains(".cat/")) {
-                splitter = urlStr.split(".cat/");
-                urlFinal = splitter[0].concat(".cat");
-            } else if (urlStr.contains(".jp/")) {
-                splitter = urlStr.split(".jp/");
-                urlFinal = splitter[0].concat(".jp");
-            } else if (urlStr.contains(".edu/")) {
-                splitter = urlStr.split(".edu/");
-                urlFinal = splitter[0].concat(".edu");
-            } else if (urlStr.contains(".us/")) {
-                splitter = urlStr.split(".us/");
-                urlFinal = splitter[0].concat(".us");
-            } else if (urlStr.contains(".uk/")) {
-                splitter = urlStr.split(".uk/");
-                urlFinal = splitter[0].concat(".uk");
-            } else if (urlStr.contains(".gov/")) {
-                splitter = urlStr.split(".gov/");
-                urlFinal = splitter[0].concat(".gov");
-            } else if (urlStr.contains(".fr/")) {
-                splitter = urlStr.split(".fr/");
-                urlFinal = splitter[0].concat(".fr");
-            } else if (urlStr.contains(".it/")) {
-                splitter = urlStr.split(".it/");
-                urlFinal = splitter[0].concat(".it");
-            } else if (urlStr.contains(".br/")) {
-                splitter = urlStr.split(".br/");
-                urlFinal = splitter[0].concat(".br");
-            } else if (urlStr.contains(".at/")) {
-                splitter = urlStr.split(".at/");
-                urlFinal = splitter[0].concat(".at");
-            } else if (urlStr.contains(".info/")) {
-                splitter = urlStr.split(".info/");
-                urlFinal = splitter[0].concat(".info");
-            } else if (urlStr.contains(".net/")) {
-                splitter = urlStr.split(".net/");
-                urlFinal = splitter[0].concat(".net");
-            } else if (urlStr.contains(".coop/")) {
-                splitter = urlStr.split(".coop/");
-                urlFinal = splitter[0].concat(".coop");
-            } else {
-                urlFinal = urlStr;
-            }
+            //Comprobamos que la URL sea correcta
+            urlFinal = this.urlConnect(urlFinal);
 
-            //Añadimos a la lista aquellas nuevas URLS y a la cola las que no se han leido
-            if (!this.list.contains(urlFinal)) {
-                this.pagesQueue.add(urlFinal);
-                this.list.add(urlFinal);
-                switch (this.list.size()) {
-                    case 100:
-                        System.out.println("100");
-                        break;
-                    case 200:
-                        System.out.println("200");
-                        break;
-                    case 500:
-                        System.out.println("500");
-                        break;
-                    case 700:
-                        System.out.println("700");
-                        break;
-                    case 1000:
-                        System.out.println("1000");
-                        break;
-                    case 1100:
-                        System.out.println("1100");
-                        break;
-                    case 1200:
-                        System.out.println("1200");
-                        break;
-                    case 1500:
-                        System.out.println("1500");
-                        break;
-                    case 2000:
-                        System.out.println("2000");
-                        break;
-                    case 2500:
-                        System.out.println("2500");
-                        break;
-                    case 3000:
-                        System.out.println("3000");
-                        break;
-                    case 4000:
-                        System.out.println("4000");
-                        break;
-                    case 4500:
-                        System.out.println("4500");
-                        break;
-                    default:
-                        break;
+            //En caso de que sea correcta lo añadimos a la lista de indexacion
+            if (urlFinal != null) {
+
+                //Añadimos a la lista aquellas nuevas URLS y a la cola las que no se han leido
+                if (!this.indexList.contains(urlFinal)) {
+                    this.indexList.add(urlFinal);
+                    
+                    //Si se encuentra en la lista de host lo metemos a la cola menos prioritaria
+                    if (this.hostList.contains(new URL(urlFinal).getHost())) {
+                        this.pagesLastQueue.add(urlFinal);
+                    } else {//En caso contrario lo añadimos a la cola de prioridad
+                        this.pagesPriorityQueue.add(urlFinal);
+                    }
+
+                    //Escribimos por pantalla el numero de paginas rastreadas
+                    this.writeStatus();
+                }
+
+                //Escribimos la URL junto con el link al que apunta
+                writeLink.write(url + "\t" + urlFinal + "\n");
+
+                //Comprobamos que no se haya llegado al limite
+                if (this.indexList.size() == this.maxDocuments) {
+                    return;
                 }
             }
+
         }
     }
+
+    /**
+     * Funcion encargada de indexar los documentos que se han rastreado con el
+     * crawler, pasando solo aquellos que se pueden indexar.
+     */
+    private void indexar() throws IOException {
+
+        //Escribimos por pantalla un mensaje de aviso
+        System.out.println("Fin del rastreo:\n\nPáginas rastreadas\tHosts diferentes\t");
+        System.out.println(this.indexList.size() + "\t\t\t\t" + this.hostList.size() + "\t");
+        System.out.println("\nSe ha alcanzado el número máximo de documentos a rastrear.");
+
+        //Cuando alcanzamos el numero maximo de documentos escribimos en un fichero
+        //todas las URLs extraidas
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("collections" + File.separator + Config.PATHS_FILE))) {
+            for (String page : this.indexList) {
+                writer.write(page + "\n");
+            }
+        }
+
+        //Escribimos por pantalla un mensaje de aviso
+        System.out.println("Se han obtenido " + this.indexList.size() + " páginas para indexar.\nIndexando...\n\n");
+
+        //Finalmente, indexamos todos los documentos
+        this.index.build("collections" + File.separator + Config.PATHS_FILE, "index/web");
+
+        //Escribimos por pantalla un mensaje de aviso
+        System.out.println("Fin de la indexación");
+    }
+
+    /**
+     * Funcion encargada de conectarse a una pagina dada la url, comprobando que
+     * sea correcta, tenga un codigo de respuesta valido y se pueda indexar.
+     *
+     * @param url url a la que queremos conectarnos
+     *
+     * @return null si ha dado algun error o la url si es correcta
+     */
+    private String urlConnect(String url) {
+        //Inicializacion de variables
+        String host, protocol, finalContent;
+        Element contentURL;
+        URL urlConnection;
+
+        try {
+
+            //Comprobamos que la URL sea correcta
+            try {
+                urlConnection = new URL(url);
+            } catch (MalformedURLException ex) {
+                return null;
+            }
+
+            //Obtenemos el host y el protocolo de la url
+            host = urlConnection.getHost();
+            protocol = urlConnection.getProtocol();
+
+            contentURL = Jsoup.parse(urlConnection, 10000);
+
+            finalContent = contentURL.html();
+
+            if (!finalContent.contains("<html") && !finalContent.contains("<!DOCTYPE html")
+                    && !finalContent.contains("<!doctype html") && !finalContent.contains("<script")
+                    && !finalContent.contains("<href")) {
+                return null;
+            }
+
+            //Comprobamos que tenga host y protocolo
+            if (host == null || protocol == null || url.length() <= 8) {
+                return null;
+            }
+
+            //Añadimos el host a la lista de hosts
+            if (!this.hostList.contains(host)) {
+                this.hostList.add(host);
+            }
+
+        } catch (IOException ex) {
+            return null;
+        }
+
+        return url;
+    }
+
+    /**
+     * Funcion encargada de escribir por pantalla el tamaño de paginas que se
+     * han rastreado hasta el momento.
+     */
+    private void writeStatus() {
+        int indexSize = this.indexList.size();
+
+        if (indexSize == 100) {
+            System.out.println("Páginas rastreadas\tHosts diferentes\t");
+        }
+
+        //Escribimos por pantalla el estado actual del tamaño de nuestra lista de urls
+        if (indexSize % 100 == 0) {
+            System.out.println(indexSize + "\t\t\t\t" + this.hostList.size() + "\t");
+        }
+
+    }
+
 }
--- src/es/uam/eps/bmi/search/test/TestCrawl.java
+++ src/es/uam/eps/bmi/search/test/TestCrawl.java
@@ -1,24 +1,58 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *
+ * Fichero TestCrawl.java.
+ *
+ *
+ * @version 1.0
+ *
+ * Created on 25/03/2019
  */
 package es.uam.eps.bmi.search.test;
 
+import es.uam.eps.bmi.search.SearchEngine;
 import es.uam.eps.bmi.search.index.WebCrawler;
+import es.uam.eps.bmi.search.index.impl.SerializedRAMIndex;
 import es.uam.eps.bmi.search.index.impl.SerializedRAMIndexBuilder;
+import es.uam.eps.bmi.search.ranking.SearchRanking;
+import es.uam.eps.bmi.search.ranking.SearchRankingDoc;
+import es.uam.eps.bmi.search.ui.TextResultDocRenderer;
+import es.uam.eps.bmi.search.vsm.DocBasedVSMEngine;
 import java.io.IOException;
 import java.net.MalformedURLException;
 
 /**
+ * Clase TestCrawl encargada de ejecutar el programa de crawling.
  *
- * @author sergio
+ * @author Miguel Alvarez Lesmes
+ * @author Sergio Romero Tapiador
+ *
  */
 public class TestCrawl {
-        public static void main (String a[]) throws IOException, MalformedURLException, MalformedURLException {
-            WebCrawler crawler= new WebCrawler("urls.txt", 50, new SerializedRAMIndexBuilder());
-            
+
+    /**
+     * Programa principal.
+     * 
+     * @param a
+     * @throws IOException
+     * @throws MalformedURLException
+     */
+    public static void main(String a[]) throws IOException, MalformedURLException {
+        //Lanzamos el crawler
+        WebCrawler crawler = new WebCrawler("urls.txt", 500, new SerializedRAMIndexBuilder());
         
+        //Creamos el motor de busqueda con la query y el limite que queramos 
+        SearchEngine engine= new DocBasedVSMEngine(new SerializedRAMIndex("index/web"));
+        String query = "sergio";
+        int cutoff=5;
+            
+        //Buscamos en el indice nuestra query predefinida y mostramos los cutoff mejores resultados
+        SearchRanking ranking = engine.search(query, cutoff);
+        System.out.println("  " + engine.getClass().getSimpleName()
+                + " + " + engine.getDocMap().getClass().getSimpleName()
+                + ": top " + cutoff + " for query '" + query + "'");
+        for (SearchRankingDoc result : ranking) {
+            System.out.println("\t" + new TextResultDocRenderer(result));
         }
+    }
 
 }
